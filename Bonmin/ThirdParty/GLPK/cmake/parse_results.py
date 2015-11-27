3eb750bb4b55d6097c3cdf915a626d4a028cc291
Yann Collette <yann.collette@artelys.com>
2015-11-27 17:03:19 +0100

    add cmakefiles for bonmin - WIP

---
+++ Bonmin/ThirdParty/GLPK/cmake/parse_results.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+import re
+import sys
+
+# Example command line
+# parse_results.py netlib_fit2d_cbc.log 'Optimal objective <number>' -68464.293294 1e-6
+# parse_results.py netlib_fit2d_cbc.log 'Optimal objective <number>' -68464.293294
+
+if (len(sys.argv) <= 4):
+    filename  = sys.argv[1]
+    pattern   = sys.argv[2]
+    ref_value = float(sys.argv[3])
+    rel_level = 1e-6
+elif (len(sys.argv) <= 5):
+    filename  = sys.argv[1]
+    pattern   = sys.argv[2]
+    ref_value = float(sys.argv[3])
+    rel_level = float(sys.argv[4])
+else:
+    print 'usage: parse_result.py filename pattern ref_value [rel_level=1e-6]'
+    sys.exit(1)
+
+# Internal variables
+number_re = '([-+]?\d+\.*\d*)'
+
+# Generate the regular expression
+pattern = pattern.replace('<number>', number_re)
+
+# Make sure file gets closed after being iterated
+with open(filename, 'r') as f:
+   # Read the file contents and generate a list with each line
+   lines = f.readlines()
+
+# Iterate each line
+for line in lines:
+    # Regex applied to each line
+    match = re.findall(pattern, line)
+    if match:
+        result = abs(float(match[0]) - ref_value) / max(abs(ref_value), 1e-9) < rel_level
+        if (not result):
+            print "FAILED"
+            sys.exit(-1)
+        else:
+            print "PASSED"
+            sys.exit()
+
+print "NOT FOUND"
+sys.exit(-1)
+
