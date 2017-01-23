19f6d37df6007076e19a64764c4f48961bae4ca9
Yann Collette <yann.collette@artelys.com>
2017-01-23 17:08:04 +0100

    fix Glpk cmakefiles

--- Cgl/ThirdParty/GLPK/config.h.cmake
+++ Cgl/ThirdParty/GLPK/config.h.cmake
@@ -1,27 +1,27 @@
 /* config.h.cmake (GLPK configuration template file) */
 
-#cmakedefine HAVE_SYS_TIME_H
+#cmakedefine HAVE_SYS_TIME_H @HAVE_SYS_TIME_H@
 /* defined if the <sys/time.h> header can be used */
 
-#cmakedefine HAVE_GETTIMEOFDAY
+#cmakedefine HAVE_GETTIMEOFDAY @HAVE_GETTIMEOFDAY@
 /* defined if the gettimeofday function can be used */
 
-#cmakedefine HAVE_GMP
+#cmakedefine HAVE_GMP @HAVE_GMP@
 /* defined if the GNU MP bignum library is available */
 /* requires <gmp.h> and -lgmp */
 
-#cmakedefine HAVE_LTDL
+#cmakedefine HAVE_LTDL @HAVE_LTDL@
 /* defined if the GNU Libtool shared library support is enabled */
 /* requires <ltdl.h> and -lltdl */
 
-#cmakedefine HAVE_DLFCN
+#cmakedefine HAVE_DLFCN @HAVE_DLFCN@
 /* defined if the POSIX shared library support is enabled */
 /* requires <dlfcn.h> */
 
-#cmakedefine ODBC_DLNAME
+#cmakedefine ODBC_DLNAME @ODBC_DLNAME@
 /* ODBC shared library name if this feature is enabled */
 
-#cmakedefine MYSQL_DLNAME
+#cmakedefine MYSQL_DLNAME @MYSQL_DLNAME@
 /* MySQL shared library name if this feature is enabled */
 
 /* eof */
