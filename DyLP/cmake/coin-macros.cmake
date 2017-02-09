eeb62a08ac79cedaf4efd0ecc54fd605e00e6b56
Yann Collette <yann.collette@artelys.com>
2017-02-09 16:04:32 +0100

    add tests for DyLP ... full of bugs ... .mps.gz files not supported

--- DyLP/cmake/coin-macros.cmake
+++ DyLP/cmake/coin-macros.cmake
@@ -93,6 +93,34 @@ macro(create_log_analysis Name AdditionalName Filename TestRegex TestRefVal Test
   set_tests_properties(${Name}_${AdditionalName} PROPERTIES PASS_REGULAR_EXPRESSION "PASSED")
 endmacro()
 
+# add_coin_dylp_test: generate a cmake wrapper around osi_dylp executable and then add the test
+# SolverName: the name of the solver. Will be appended to the out and log filename (must have the same name as the built target)
+# Name: the name of the test
+# FileData: the name of the mps / lp data file
+# FileOut: the name of the .out data file
+# FileLog: the name of the .log data file
+
+macro(add_coin_dylp_test Name SolverName FileData FileOut FileLog)
+  # -solution ${FileOut} -solve
+  message(STATUS "DEBUG: FileData = ${FileData}")
+  if (WIN32)
+    file(WRITE ${CMAKE_BINARY_DIR}/CoinTests/${Name}_${SolverName}.cmake
+         "execute_process(COMMAND     ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/osi_dylp.exe -e ${CMAKE_SOURCE_DIR}/DyLP/src/Dylp/dy_errmsgs.txt ${FileData} \n"
+         "                OUTPUT_FILE ${FileLog})\n")
+  else ()
+    file(WRITE ${CMAKE_BINARY_DIR}/CoinTests/${Name}_${SolverName}.cmake
+         "execute_process(COMMAND     ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/osi_dylp -e ${CMAKE_SOURCE_DIR}/DyLP/src/Dylp/dy_errmsgs.txt ${FileData} \n"
+         "                OUTPUT_FILE ${FileLog})\n")
+  endif ()
+  
+  add_test(NAME ${Name}
+           COMMAND ${CMAKE_COMMAND} -P ${CMAKE_BINARY_DIR}/CoinTests/${Name}_${SolverName}.cmake)
+  
+  if (WIN32)
+    set_tests_properties(${Name} PROPERTIES ENVIRONMENT "PATH=${CMAKE_BINARY_DIR}/Dependencies/lib\\;${CMAKE_BINARY_DIR}/Dependencies/bin")
+  endif ()
+endmacro()
+
 # From hydrogen CMakeLists.txt file
 string( ASCII 27 _escape)
 
