ef10a7f8029c636b887755db9a7a51f241eba26d
Yann Collette <yann.collette@artelys.com>
2018-07-05 10:16:43 +0200

    fix a warning

--- Cbc/cmake/compat.c
+++ Cbc/cmake/compat.c
@@ -7,6 +7,7 @@ extern "C" {
   
 // Prior to GLIBC_2.14, memcpy was aliased to memmove.
 void* memmove(void* a, const void* b, size_t c);
+void* memcpy(void* a, const void* b, size_t c);
 void* memcpy(void* a, const void* b, size_t c) {
   return memmove(a, b, c);
 }
